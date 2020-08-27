8eacc1dd880b85c689b68d420696b14de9953210
Ben <bdye71@gmail.com>
2020-08-27 02:39:16 -0400

    Native C run time-recording program

--- time_c_fib.c
+++ time_c_fib.c
@@ -8,14 +8,36 @@ int main(int argc, char *argv[])
 {
 	int n = atoi(argv[1]);
 	printf("Computing first %d Fibonacci numbers.\n", n);
+	double results[n]; /* Array of size n to store results */
 	int i;
-	//time_t start;
-	//time(&start);
-	for (i = 0; i < n; i++)
-		printf("%lu\n", fib(i));
-	//time_t end;
-	//time(&end);
-	//printf("%s\n", ctime(&start));
-	//printf("%s\n", ctime(&end));
+	clock_t processor_start = clock();
+	
+	for (i = 0; i < n; i++) {
+		clock_t start = clock();
+		//time_t start;
+		//time(&start);
+		fib(i);
+		clock_t end = clock();
+		//time_t
+		//time(&end);
+		results[i] = (end - start) / CLOCKS_PER_SEC;
+	};
+	
+	clock_t processor_end = clock();
+	printf("processor_end - processor_start = %ld clock cycles\n",
+	processor_end - processor_start);
+	printf("CLOCKS_PER_SEC constant defined as %ld\n", CLOCKS_PER_SEC);
+	long total_seconds = (processor_end - processor_start) / CLOCKS_PER_SEC; 
+		/* CLOCKS_PER_SEC constant is already #define d in <time.h> */
+	printf("total seconds = clocks / CLOCKS_PER_SEC = %ld\n", total_seconds);
+	
+	FILE *fp = fopen("results pure C.txt", "w");
+	for (i=0; i < n; i++){
+		printf("%f\n", results[i]);
+		//fgets(i);
+		fprintf(fp, "%d: %f\n", i, results[i]);
+		//fprintf(fp, "\n");
+	};
+	fclose(fp);
 	return 0;	
 }

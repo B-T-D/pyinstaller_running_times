e7fc0eb2d74c218521b1bfe4db77c9c64291c5f1
Ben <bdye71@gmail.com>
2020-08-27 21:53:44 -0400

    time_c_fib.c refactored some, and reduced rounding of low output values to zero

--- time_c_fib.c
+++ time_c_fib.c
@@ -5,89 +5,47 @@
 
 # include "fib.c"
 
-# define TIMESTAMP_SIZE 17
-# define STAMPED_FILENAME_SIZE 34
 # define FILENAME_SIZE 19
 
-void timestamp_filename(char *filename)
-/* Return string for use as differentiable filename, based on the current time. */
+void 
+time_trial(int n, double *results_array)
+/* Run trials for Fibonacci numbers from 0 to n-1 and return the results stored in array.*/
 {
-		//char filename[STAMPED_FILENAME_SIZE];
-		printf("filename: %s\n", filename);
-		time_t stamp; /* Create timestamp string */
-		printf("ok\n");
-		struct tm *pointer_to_stamp;
-		printf("still ok\n");
-		stamp = time(NULL);
-		size_t smax = 15; /* Max of 15 characters needed for the timestamp part of the filename,
-		'YYYY-MM-DD HHMM" */
-		char stampstring[TIMESTAMP_SIZE];
-		strftime(filename, sizeof(stampstring), "%Y-%m-%d %H%M", localtime(&stamp));
-		printf("filename: %s\n", filename);
-		strcat(filename, " results pure C.txt");
-		return filename;
+	int i;
+	for (i=0; i<n; i++) {
+		clock_t start = clock();
+		fib(i);
+		clock_t end = clock();
+		results_array[i] = (( double )(end - start)) / CLOCKS_PER_SEC;
+			/* Cast the quantity (end - start) to a double, otherwise too many 
+			of the output values round to zero after dividing by CLOCKS_PER SEC,
+			especially when CLOCKS_PER_SEC is 1 million */
+	};
 }
 
 int main(int argc, char *argv[])
 {
 	int n = atoi(argv[1]);
 	printf("Computing first %d Fibonacci numbers.\n", n);
-	double *results = malloc(n * sizeof(results)); /* Must be done dynamically with malloc, 
-	else it won't compile on Windows. GCC lets you use double my_array_of_doubles[n] for a 
-	dynamically changeable value of n, but VS compiler doesn't. */
-	int i;
-	clock_t processor_start = clock();
 	
-	for (i = 0; i < n; i++) {
-		clock_t start = clock();
-		//time_t start;
-		//time(&start);
-		fib(i);
-		clock_t end = clock();
-		//time_t
-		//time(&end);
-		results[i] = (end - start) / CLOCKS_PER_SEC;
-	};
+	double *results = malloc(n * sizeof(results)); /* Initialize array of size n */
+		/* Must be done dynamically with malloc, otherwise won't compile on Windows.
+		GCC allows "double results[n]" without a constant value for n, but VS compiler
+		doesn't.*/
 	
-	clock_t processor_end = clock();
-	printf("processor_end - processor_start = %ld clock cycles\n",
-	processor_end - processor_start);
-	printf("CLOCKS_PER_SEC constant defined as %ld\n", CLOCKS_PER_SEC);
-	long total_seconds = (processor_end - processor_start) / CLOCKS_PER_SEC; 
-		/* CLOCKS_PER_SEC constant is already #define d in <time.h> */
-	printf("total seconds = clocks / CLOCKS_PER_SEC = %ld\n", total_seconds);
+	time_trial(n, results);	
 	
-	/* Create differentiable filename based on current time */
-	//char filename[FILENAME_SIZE];
-	//time_t stamp; /* timestamp for use in results filename */
-	//struct tm *pointer_to_stamp;
-	//stamp = time(NULL);
-	//size_t smax = 15; /* max of 15 characters for the filename timestamp */
-	//char stampstring[TIMESTAMP_SIZE];
-	//strftime(filename, sizeof(stampstring), "%Y-%m-%d %H%M", localtime(&stamp));
-	
-	//strcat(filename, " results pure C.txt");
-	
-	//char tstamp_fname[STAMPED_FILENAME_SIZE];
-	//timestamp_filename(tstamp_fname);
-	
-	//printf("%s\n", tstamp_fname);
-	
-	/* Write results array to individualized file */
+	/* Write results to file */
 	char filename[FILENAME_SIZE];
-	strcat(filename, "results pure C.txt");
+	strcat(filename, "results pure C.txt"); /* file with stable name that Python script 
+		knows */
 	FILE *fp = fopen(filename, "w");
+	int i;
 	for (i=0; i < n; i++){
-		printf("%f\n", results[i]);
-		//fgets(i);
+		//printf("%f\n", results[i]);
 		fprintf(fp, "%d: %f\n", i, results[i]);
-		//fprintf(fp, "\n");
 	};
 	fclose(fp);
-	
-	/* Overwrite the "latest" file with the new results--the file whose
-	name the Python dict converter knows. */
-	
 	
 	return 0;	
 }
